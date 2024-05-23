e467c974d0a60c7b59df0e5d2f132f4cdc94f02b
Blankaert <blankaertlouis@outlook.com>
2024-05-24 00:41:17 +0200

    rajouter les type et la galery

--- app/db/firebaseConfig.js
+++ app/db/firebaseConfig.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import {getFirestore} from "firebase/firestore"
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_API_KEY,
-  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_SENDER_ID,
-  appId: import.meta.env.VITE_APP_ID
+  apiKey: process.env.VITE_API_KEY,
+  authDomain: process.env.VITE_AUTH_DOMAIN,
+  projectId: process.env.VITE_PROJECT_ID,
+  storageBucket: process.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: process.env.VITE_SENDER_ID,
+  appId: process.env.VITE_APP_ID
 };
 
 export const app = initializeApp(firebaseConfig);
--- app/page.jsx
+++ app/page.jsx
@@ -1,13 +1,48 @@
 "use client"
 
-import { useState, useEffect } from "react";
+import { useEffect, useState } from 'react';
+import { collection, getDocs } from 'firebase/firestore';
+import { db } from './db/firebaseConfig';
+import Galery from './Components/Galery';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import Header from "./Components/Header";
 
+// Déclaration de l'objet ProductsTypes comme un objet JavaScript
+const ProductsTypes = {
+  id: "",
+  name: "",
+  desc: "",
+  country: "",
+  image: "",
+  price: 0,
+  quantityPack: 0,
+  strength: 0,
+  quantityProduct: 0,
+};
+
 export default function Home() {
+  const [dataProducts, setDataProducts] = useState([]);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    const productsCollection = collection(db, 'products');
+    const productsSnapshot = await getDocs(productsCollection);
+    const productsData = productsSnapshot.docs.map((doc) => ({
+      id: doc.id,
+      ...doc.data()
+    }));
+    setDataProducts(productsData);
+  };
 
   return (
     <>
-      <Header/>
+      <ToastContainer />
+      <Header />
+      <Galery dataProducts={dataProducts} />
     </>
   );
 }
